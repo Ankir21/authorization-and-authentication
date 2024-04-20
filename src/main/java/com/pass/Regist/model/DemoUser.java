0eb78e5cb61a96abf8379ad8bfb249cf5d0581f7
Ankir21 <ankirss7@gmail.com>
2024-04-20 17:04:28 +0500

    обавлены все файлы проекта

---
+++ src/main/java/com/pass/Regist/model/DemoUser.java
@@ -0,0 +1,9 @@
+package com.pass.Regist.model;
+
+public class DemoUser {
+    public void demo(){
+        User user = new User();
+        user.setFirstName("Name");
+        user.setPassword("my password");
+    }
+}
---
+++ src/main/java/com/pass/Regist/repo/DemoRepo.java
@@ -0,0 +1,10 @@
+package com.pass.Regist.repo;
+
+import com.pass.Regist.model.User;
+
+public class DemoRepo {
+    public void demo(){
+        User user = new User();
+        user.
+    }
+}
